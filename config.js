2a8cbba23446a0e433a3700fd353bcc160586183
h <chedirebvo@gmail.com>
2025-05-23 12:58:22 +0200

    Update config.js

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ global.botnumber = "" //Inserisci nelle apici il numero del bot (es. +3937620304
 global.confirmCode = ""
 
 global.owner = [
-    ['393762030481', 'Proprietario', true],
+    ['393780870234', 'Proprietario', true],
 ] //Numero degli owner
 
 //────────────────────────────
